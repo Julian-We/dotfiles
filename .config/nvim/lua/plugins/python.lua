7a651f124233995704e78a34ba4efcba11171833
Julian Wegner <wegner.julian@pm.me>
2026-04-20 13:46:28 +0200

    copied entire .config folder again

--- .config/nvim/lua/plugins/python.lua
+++ .config/nvim/lua/plugins/python.lua
@@ -66,4 +66,56 @@ return {
       },
     },
   },
+
+  -- molten.nvim for Jupyter notebook integration
+  {
+    "benlubas/molten-nvim",
+    version = "^1.0.0",
+    build = ":UpdateRemotePlugins",
+    dependencies = {
+      "3rd/image.nvim", -- optional: for image rendering
+    },
+    ft = { "python", "markdown" },
+    keys = {
+      { "<leader>mi", "<cmd>MoltenInit<cr>", desc = "Initialize Molten", ft = { "python", "markdown" } },
+      { "<leader>me", "<cmd>MoltenEvaluateOperator<cr>", desc = "Evaluate Operator", ft = { "python", "markdown" } },
+      { "<leader>ml", "<cmd>MoltenEvaluateLine<cr>", desc = "Evaluate Line", ft = { "python", "markdown" } },
+      { "<leader>mr", "<cmd>MoltenReevaluateCell<cr>", desc = "Re-evaluate Cell", ft = { "python", "markdown" } },
+      { "<leader>mv", ":<C-u>MoltenEvaluateVisual<cr>", mode = "v", desc = "Evaluate Visual", ft = { "python", "markdown" } },
+      { "<leader>md", "<cmd>MoltenDelete<cr>", desc = "Delete Cell", ft = { "python", "markdown" } },
+      { "<leader>mh", "<cmd>MoltenHideOutput<cr>", desc = "Hide Output", ft = { "python", "markdown" } },
+      { "<leader>mo", "<cmd>MoltenShowOutput<cr>", desc = "Show Output", ft = { "python", "markdown" } },
+    },
+    init = function()
+      -- Molten configuration
+      vim.g.molten_auto_open_output = false
+      vim.g.molten_image_provider = "image.nvim"
+      vim.g.molten_output_win_max_height = 20
+      vim.g.molten_wrap_output = true
+      vim.g.molten_virt_text_output = true
+      vim.g.molten_virt_lines_off_by_1 = true
+    end,
+  },
+
+  -- image.nvim for displaying images in molten output (optional)
+  {
+    "3rd/image.nvim",
+    optional = true,
+    opts = {
+      backend = "kitty", -- or "ueberzug" depending on your terminal
+      integrations = {
+        markdown = {
+          enabled = true,
+          clear_in_insert_mode = false,
+          download_remote_images = true,
+          only_render_image_at_cursor = false,
+        },
+      },
+      max_width = 100,
+      max_height = 12,
+      max_height_window_percentage = math.huge,
+      max_width_window_percentage = math.huge,
+      window_overlap_clear_enabled = true,
+    },
+  },
 }
